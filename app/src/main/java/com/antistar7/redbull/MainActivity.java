c9969a521a22e9650eeb747cb2ada876eb87f42f
SSG\q93lbz <q93lbz@shinsegae.com>
2024-01-18 10:58:16 +0900

    메인 추가

---
+++ app/src/main/java/com/antistar7/redbull/MainActivity.java
@@ -0,0 +1,28 @@
+package com.antistar7.redbull;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.antistar7.redbull.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
